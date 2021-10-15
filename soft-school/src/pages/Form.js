144e16f688c048d96245cc356ef93321e18f2562
Josan Johnata <josanjohnata@gmail.com>
2021-10-15 17:49:48 -0300

    Finished teh landing page

--- soft-school/src/pages/Form.js
+++ soft-school/src/pages/Form.js
@@ -1,26 +1,44 @@
 import React from "react";
 import "../style/Form.css";
-// import ImgContact from '../images/Contato.png';
+import ImgContact from "../images/contatoV2.jpg";
+import Footer from "../pages/Footer";
 
 export default function Form() {
   return (
-    <div className="formContainer">
-      <h2>EVOLUA AO NOSSO LADO!</h2>
-      <p>Preencha o formulário e trilhe um caminho de sucesso com a gente.</p>
-      <form className="formMessage">
-        <label for="name">Nome: </label>
-        <input name="name" type="text" placeholder="Digite seu nome" />
-        <label for="empresa">Empresa: </label>
-        <input name="empresa" type="text" placeholder="Digite o Nome da Empresa" />
-        <label for="email">E-mail: </label>
-        <input name="email" type="email" placeholder="Digite seu E-mail" />
-        <label for="telefone">Telefone: </label>
-        <input name="telefone" type="telefone" placeholder="Digite seu Telefone" />
-        <button id="buttonEnviar" type="button">
-          Enviar
-        </button>
-      </form>
-      {/* <ImgContact /> */}
-    </div>
+    <>
+      <div className="formContainer">
+        <h2>EVOLUA AO NOSSO LADO!</h2>
+        <p>Preencha o formulário e trilhe um caminho de sucesso com a gente.</p>
+        <div className="formGrid">
+          <form className="formMessage">
+            <label for="name">Nome: </label>
+            <input name="name" type="text" placeholder="Digite seu nome" />
+            <label for="empresa">Empresa: </label>
+            <input
+              name="empresa"
+              type="text"
+              placeholder="Digite o Nome da Empresa"
+            />
+            <label for="email">E-mail Corporativo: </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="Digite seu E-mail Corporativo"
+            />
+            <label for="telefone">Telefone: </label>
+            <input
+              name="telefone"
+              type="telefone"
+              placeholder="Digite seu Telefone"
+            />
+            <button id="buttonEnviar" type="button">
+              Enviar
+            </button>
+          </form>
+        <img src={ImgContact} />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
