55cadd6cfce63fa29fb73ec718dfdd98428fbd4b
Josan Johnata <josanjohnata@gmail.com>
2021-10-15 23:53:30 -0300

    Finished login popup

--- soft-school/src/components/NavBar.js
+++ soft-school/src/components/NavBar.js
@@ -1,32 +1,43 @@
-import React from "react";
-import '../style/NavBar.css'
-import {
-  Navbar,
-  Nav,
-  Container,
-  Col,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import "../style/NavBar.css";
+import { Navbar, Nav, Container, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Logo from '../images/logo.jpg'
+import Logo from "../images/logo.jpg";
+import PopupLogin from "./PopupsLogin";
 
 function NavBar() {
+  const [buttonLogin, setButtonLogin] = useState(false);
+
   return (
     <>
       <Navbar expand="lg" className="containerNavBar" collapseOnSelect>
-      <img src={Logo} className="logo" />
-        <Container fluid className="text-justify">        
-          <Col md="auto">          
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggle-navbar" />
+        <img src={Logo} className="logo" />
+        <Container fluid className="text-justify">
+          <Col md="auto">
+            <Navbar.Toggle
+              aria-controls="responsive-navbar-nav"
+              className="toggle-navbar"
+            />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link className="navBarItems" href="#details">O que são Soft Skills</Nav.Link>
-                <Nav.Link className="navBarItems" href="#BigPac">Nosso plus</Nav.Link>
-                <Nav.Link className="navBarItems" href="#footert">Quem Somos</Nav.Link>
-                <Nav.Link className="navBarItems" href="#contact">Contato</Nav.Link>
-              </Nav>                  
-                <Nav.Link eventKey={2} href="#memes" id="login">
-                  Login
+                <Nav.Link className="navBarItems" href="#details">
+                  O que são Soft Skills
                 </Nav.Link>
+                <Nav.Link className="navBarItems" href="#BigPac">
+                  Nosso plus
+                </Nav.Link>
+                <Nav.Link className="navBarItems" href="#footert">
+                  Quem Somos
+                </Nav.Link>
+                <Nav.Link className="navBarItems" href="#contact">
+                  Contato
+                </Nav.Link>
+              </Nav>
+              <Nav.Link onClick={() => setButtonLogin(true)} id="login">
+                Login
+              </Nav.Link>
+              <PopupLogin trigger={buttonLogin} setTrigger={setButtonLogin}>                
+              </PopupLogin>
             </Navbar.Collapse>
           </Col>
         </Container>
